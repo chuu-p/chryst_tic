3d9c5a21fb9974ecad6eb480bc18c1dca4138c12
chuu-p <chuu801@pm.me>
2025-12-06 18:47:00 +0100

    add resources :smiley:

--- src/lib.rs
+++ src/lib.rs
@@ -3,41 +3,68 @@ mod tic80;
 
 use tic80::*;
 
+trait Drawable {
+    fn draw(&self);
+}
+
+struct DebugGuy {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Drawable for DebugGuy {
+    fn draw(&self) {
+        unsafe {
+            spr(
+                1 + T % 60 / 30 * 2,
+                self.x,
+                self.y,
+                SpriteOptions {
+                    w: 2,
+                    h: 2,
+                    transparent: &[14],
+                    scale: 3,
+                    ..Default::default()
+                },
+            );
+        }
+    }
+}
+
 static mut T: i32 = 0;
-static mut X: i32 = 96;
-static mut Y: i32 = 24;
+
+static mut DEBUG_GUY: DebugGuy = DebugGuy { x: 96, y: 24 };
 
 #[export_name = "TIC"]
 pub fn tic() {
     if btn(0) {
-        unsafe { Y -= 1 }
+        unsafe { DEBUG_GUY.y -= 1 }
     }
     if btn(1) {
-        unsafe { Y += 1 }
+        unsafe { DEBUG_GUY.y += 1 }
     }
     if btn(2) {
-        unsafe { X -= 1 }
+        unsafe { DEBUG_GUY.x -= 1 }
     }
     if btn(3) {
-        unsafe { X += 1 }
+        unsafe { DEBUG_GUY.x += 1 }
     }
 
     cls(13);
+
     unsafe {
-        spr(
-            1 + T % 60 / 30 * 2,
-            X,
-            Y,
-            SpriteOptions {
-                w: 2,
-                h: 2,
-                transparent: &[14],
-                scale: 3,
-                ..Default::default()
-            },
-        );
+        DEBUG_GUY.draw();
     }
-    print!("HELLO WORLD!", 84, 84, PrintOptions::default());
+
+    print!(
+        "HELLO DAVE!",
+        84,
+        84,
+        PrintOptions {
+            small_font: true,
+            ..Default::default()
+        }
+    );
 
     unsafe {
         T += 1;

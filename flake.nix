0fea70aeb9adc993f169b24784e37a88dcc3823f
chuu-p <chuu801@pm.me>
2025-12-06 12:12:20 +0100

    cleanup :smiley:

--- flake.nix
+++ flake.nix
@@ -10,38 +10,43 @@
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, ... }:
-    let
-      rustVersion = "latest"; # or "1.62.0"
-
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        overlays = [
-          rust-overlay.overlays.default
-          (final: prev: {
-            my-rust = prev.rust-bin.stable.${rustVersion}.default.override {
-              extensions = [
-                "rust-src"
-                "rust-analyzer"
-              ];
-              targets = [ "wasm32-unknown-unknown" ];
-            };
-          })
-        ];
-      };
-    in
-    {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        strictDeps = true;
-
-        nativeBuildInputs = with pkgs;[
-          my-rust
-          pkg-config
-          binaryen
-        ];
-
-        RUST_BACKTRACE = "1";
-      };
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    ...
+  }: let
+    tic80 = pkgs.callPackage ./tic80.nix {
+      withPro = true;
     };
-}
+    rustVersion = "latest"; # or "1.62.0"
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [
+        rust-overlay.overlays.default
+        (final: prev: {
+          my-rust = prev.rust-bin.stable.${rustVersion}.default.override {
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+            ];
+            targets = ["wasm32-unknown-unknown"];
+          };
+        })
+      ];
+    };
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      strictDeps = true;
 
+      nativeBuildInputs = with pkgs; [
+        my-rust
+        pkg-config
+        binaryen
+        tic80
+      ];
+
+      RUST_BACKTRACE = "1";
+    };
+  };
+}

d59e898e8c5441c0dec33773a8e9f11f7602d1ee
chuu-p <chuu801@pm.me>
2026-04-18 15:23:47 +0200

    chryst working :sparkles:

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 mod alloc;
 mod tic80;
 
-use std::collections::HashMap;
-
 use itertools::izip;
+use shipyard::{Component, IntoIter, View, World};
+use std::collections::HashMap;
 use tic80::*;
 
 enum Color {
@@ -14,7 +14,7 @@ enum Color {
     Red = 4,
 }
 
-static PALETTE_LC: &str = "313432323e42454b4b3a5f3b7c4545675239625055516b43796c647182459e805c998579ac9086a6a296bcb7a500ffff";
+static PALETTE_LC: &str = "000000fffffff7d100e86800c0d725abcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdef";
 fn load_palette(palette: &str) {
     let mut map: HashMap<i32, String> = HashMap::new();
     for i in 0..16 {
@@ -33,8 +33,7 @@ fn load_palette(palette: &str) {
     }
 }
 
-use shipyard::{Component, IntoIter, View, World};
-
+// components
 #[derive(Component, Debug, Clone)]
 pub struct Position {
     pub x: f32,
@@ -53,10 +52,25 @@ pub struct Transmission {
     pub radius: f32,
 }
 
+#[derive(Debug, Clone)]
+pub enum ScriptState {
+    Idle,
+    Running,
+    Starting,
+    Stopped
+}
+
+#[derive(Component, Debug, Clone)]
+pub struct Script {
+    pub script_source: String,
+    pub script_state: ScriptState,
+}
+
 pub struct GameWorld {
     pub world: World,
 }
 
+// systems 
 impl GameWorld {
     pub fn new() -> Self {
         let mut world = World::new();
@@ -67,11 +81,87 @@ impl GameWorld {
                 name: "Beisfrost".to_string(),
                 enabled: true,
             },
-            Position { x: 0.0, y: 0.0 },
+            Position { x: 120.0, y: 60.0 },
             Transmission { radius: 10.0 },
+            Script {
+                script_source: "".to_string(),
+                script_state: ScriptState::Stopped
+            }
         ));
 
         Self { world }
+    }
+}
+
+// entities 
+unsafe fn render_nodes() {
+    let world = WORLD.as_mut().unwrap();
+
+    let (entity_id, position_data): (Option<shipyard::EntityId>, (f32, f32)) = {
+        let positions = world.world.borrow::<View<Position>>().unwrap();
+        positions
+            .iter()
+            .with_id()
+            .next()
+            .map(|(id, p)| (Some(id), (p.x, p.y)))
+            .unwrap_or((None, (0.0, 0.0)))
+    };
+
+    if let Some(entity) = entity_id {
+        if btn(0) {
+            world.world.get::<&mut Position>(entity).unwrap().y -= 1.0;
+        }
+        if btn(1) {
+            world.world.get::<&mut Position>(entity).unwrap().y += 1.0;
+        }
+        if btn(2) {
+            world.world.get::<&mut Position>(entity).unwrap().x -= 1.0;
+        }
+        if btn(3) {
+            world.world.get::<&mut Position>(entity).unwrap().x += 1.0;
+        }
+    }
+    let (node_data, position_data, transmission_data): (Vec<_>, Vec<_>, Vec<_>) = {
+        let nodes = world.world.borrow::<View<Node>>().unwrap();
+        let positions = world.world.borrow::<View<Position>>().unwrap();
+        let transmissions = world.world.borrow::<View<Transmission>>().unwrap();
+        (
+            nodes.iter().cloned().collect(),
+            positions.iter().cloned().collect(),
+            transmissions.iter().cloned().collect(),
+        )
+    };
+
+    for (node, position, transmission) in izip!(
+        node_data.iter(),
+        position_data.iter(),
+        transmission_data.iter()
+    ) {
+        circb(
+            position.x as i32,
+            position.y as i32,
+            transmission.radius as i32,
+            1,
+        );
+        let width = print_alloc(&node.name, 1024, 1024, Default::default());
+
+        let opts = PrintOptions {
+            color: 1,
+            ..Default::default()
+        };
+
+        print!(
+            format!("{},{}", position.x, position.y),
+            position.x as i32,
+            position.y as i32,
+            PrintOptions::default()
+        );
+        print!(
+            node.name.to_string(),
+            position.x as i32,
+            position.y as i32 - 8,
+            PrintOptions::default()
+        );
     }
 }
 
@@ -80,10 +170,7 @@ static mut T: i32 = 0;
 static mut WORLD: Option<GameWorld> = None;
 
 pub fn init() {
-    // TODO load palette and show a rectangle with each color
-    // TODO show fps and ms per frame
-    // TODO show x,y of Beisfost at top right
-    // load_palette(PALETTE_LC);GameWorld::new()
+    load_palette(PALETTE_LC);
     unsafe {
         WORLD = Some(GameWorld::new());
     }
@@ -92,73 +179,30 @@ pub fn init() {
 #[export_name = "TIC"]
 pub fn tic() {
     unsafe {
+        let start = tic80::time();
+
         if T == 0 {
             init();
         }
 
-        let world = WORLD.as_mut().unwrap();
-
-        let (entity_id, position_data): (Option<shipyard::EntityId>, (f32, f32)) = {
-            let positions = world.world.borrow::<View<Position>>().unwrap();
-            positions
-                .iter()
-                .with_id()
-                .next()
-                .map(|(id, p)| (Some(id), (p.x, p.y)))
-                .unwrap_or((None, (0.0, 0.0)))
-        };
-
-        if let Some(entity) = entity_id {
-            if btn(0) {
-                world.world.get::<&mut Position>(entity).unwrap().y -= 1.0;
-            }
-            if btn(1) {
-                world.world.get::<&mut Position>(entity).unwrap().y += 1.0;
-            }
-            if btn(2) {
-                world.world.get::<&mut Position>(entity).unwrap().x -= 1.0;
-            }
-            if btn(3) {
-                world.world.get::<&mut Position>(entity).unwrap().x += 1.0;
-            }
-        }
-
         cls(0);
 
-        let (node_data, position_data, transmission_data): (Vec<_>, Vec<_>, Vec<_>) = {
-            let nodes = world.world.borrow::<View<Node>>().unwrap();
-            let positions = world.world.borrow::<View<Position>>().unwrap();
-            let transmissions = world.world.borrow::<View<Transmission>>().unwrap();
-            (
-                nodes.iter().cloned().collect(),
-                positions.iter().cloned().collect(),
-                transmissions.iter().cloned().collect(),
-            )
-        };
+        // render_nodes();
 
-        for (node, position, transmission) in izip!(
-            node_data.iter(),
-            position_data.iter(),
-            transmission_data.iter()
-        ) {
-            circb(
-                position.x as i32,
-                position.y as i32,
-                transmission.radius as i32,
-                1,
-            );
-            let width = print_alloc(&node.name, 1024, 1024, Default::default());
+        // Render Editor
 
-            let opts = PrintOptions {
+        let end = tic80::time();
+        let elapsed = end - start;
+        print!(
+            format!("ms/f:{:.2}", elapsed),
+            WIDTH - 70,
+            8,
+            PrintOptions {
                 color: 1,
                 ..Default::default()
-            };
+            }
+        );
 
-            print!(
-                node.name.to_string(),
-                position.x as i32, position.y as i32, opts
-            );
-        }
         T += 1;
     }
 }

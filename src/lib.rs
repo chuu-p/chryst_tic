dc70e94de8fe437d5803a28c04ca2a17e22feb11
chuu-p <chuu801@pm.me>
2026-04-19 00:54:50 +0200

    chryst backup :sparkles:

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,23 @@ mod tic80;
 
 use itertools::izip;
 use shipyard::{Component, IntoIter, View, World};
-use std::collections::HashMap;
 use tic80::*;
+
+use rhai::Engine;
+use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+enum Button {
+    Up = 0,
+    Down = 1,
+    Left = 2,
+    Right = 3,
+    A = 4,
+    B = 5,
+    X = 6,
+    Y = 7,
+}
 
 enum Color {
     Black = 0,
@@ -16,11 +31,6 @@ enum Color {
 
 static PALETTE_LC: &str = "000000fffffff7d100e86800c0d725abcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdef";
 fn load_palette(palette: &str) {
-    let mut map: HashMap<i32, String> = HashMap::new();
-    for i in 0..16 {
-        map.insert(i, "AABBCC".to_string());
-    }
-
     for i in 0..16 {
         let r = u8::from_str_radix(&palette[i * 6..i * 6 + 2], 16).unwrap();
         let g = u8::from_str_radix(&palette[i * 6 + 2..i * 6 + 4], 16).unwrap();
@@ -57,43 +67,133 @@ pub enum ScriptState {
     Idle,
     Running,
     Starting,
-    Stopped
+    Stopped,
 }
 
-#[derive(Component, Debug, Clone)]
-pub struct Script {
-    pub script_source: String,
-    pub script_state: ScriptState,
+#[derive(Component, Clone, Debug)]
+pub struct CodeRunner {
+    sender: Arc<Mutex<mpsc::Sender<MessageType>>>,
+    receiver: Arc<Mutex<mpsc::Receiver<MessageType>>>,
+    script: String,
+    script_state: ScriptState,
+}
+
+#[derive(Debug, Clone)]
+enum MessageType {
+    Ping(PingMessage),
+    Exit,
+}
+
+#[derive(Debug, Clone)]
+struct PingMessage {
+    text: String,
 }
 
 pub struct GameWorld {
     pub world: World,
 }
 
-// systems 
+fn factory_node() -> (Node, Position, Transmission, CodeRunner) {
+    let script = r#"
+             loop {
+                 let input = wait_for_input();
+                 print("eb received: " + input);
+             }
+         "#
+    .to_string();
+    // let script = r#"
+    //          loop {
+    //              let input = wait_for_input();
+    //              if input == "exit" {
+    //                  print("Exiting Rhai loop!");
+    //                  break;
+    //              }
+    //              print("eb received: " + input);
+    //          }
+    //      "#
+    // .to_string();
+    let script_clone = script.clone();
+    let script_state = ScriptState::Stopped;
+    let (tx, rx) = mpsc::channel::<MessageType>();
+    let receiver = Arc::new(Mutex::new(rx));
+    let sender = Arc::new(Mutex::new(tx));
+
+    // let script_clone = script.clone();
+    let rx_clone = receiver.clone();
+    let tx_clone = sender.clone();
+    let _handle = thread::spawn(move || {
+        let mut engine = Engine::new();
+        // Register our blocking receive function
+        engine.register_fn("wait_for_input", move || -> MessageType {
+            let rx = rx_clone
+                .lock()
+                .unwrap_or_else(|e| panic!("Failed to lock receiver: {}", e));
+            rx.recv().unwrap_or_else(|_| MessageType::Exit)
+        });
+        engine.register_fn("transmit", move |message: MessageType| -> MessageType {
+            let tx = tx_clone
+                .lock()
+                .unwrap_or_else(|e| panic!("Failed to lock sender: {}", e));
+            tx.send(message)
+                .unwrap_or_else(|e| panic!("Failed to send message: {}", e));
+            MessageType::Exit
+        });
+        engine
+            .eval::<()>(&script_clone)
+            .unwrap_or_else(|e| panic!("Script evaluation failed: {}", e));
+    });
+
+    (
+        Node {
+            id: 1,
+            name: "Beisfrost".to_string(),
+            enabled: true,
+        },
+        Position { x: 120.0, y: 60.0 },
+        Transmission { radius: 10.0 },
+        CodeRunner {
+            sender,
+            receiver,
+            script,
+            script_state,
+        },
+    )
+}
+
 impl GameWorld {
     pub fn new() -> Self {
         let mut world = World::new();
 
-        world.add_entity((
-            Node {
-                id: 1,
-                name: "Beisfrost".to_string(),
-                enabled: true,
-            },
-            Position { x: 120.0, y: 60.0 },
-            Transmission { radius: 10.0 },
-            Script {
-                script_source: "".to_string(),
-                script_state: ScriptState::Stopped
-            }
-        ));
+        world.add_entity(factory_node());
 
         Self { world }
     }
 }
+unsafe fn debug_send_message() {
+    let world = WORLD.as_mut().unwrap();
 
-// entities 
+    let (entity_id, position_data): (Option<shipyard::EntityId>, (f32, f32)) = {
+        let positions = world.world.borrow::<View<Position>>().unwrap();
+        positions
+            .iter()
+            .with_id()
+            .next()
+            .map(|(id, p)| (Some(id), (p.x, p.y)))
+            .unwrap_or((None, (0.0, 0.0)))
+    };
+
+    if let Some(entity) = entity_id {
+        let bf = world.world.get::<&mut CodeRunner>(entity).unwrap();
+        bf.sender
+            .lock()
+            .unwrap()
+            .send(MessageType::Ping(PingMessage {
+                text: "A".to_string(),
+            }))
+            .unwrap();
+    }
+}
+
 unsafe fn render_nodes() {
     let world = WORLD.as_mut().unwrap();
 
@@ -188,6 +288,9 @@ pub fn tic() {
         cls(0);
 
         // render_nodes();
+        if btnp(Button::A as i32, 120, 6) {
+            debug_send_message()
+        }
 
         // Render Editor
 
